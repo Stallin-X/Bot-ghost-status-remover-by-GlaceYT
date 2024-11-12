5c4883c11cb878d984701da350a544cbfba919d0
Stallin-X <aarnavjoby62@gmail.com>
2024-11-12 17:20:26 +0530

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 Listening to Titan Customix", "🎮 Playing Grand Theft Auto: San Andreas", "🤖 Watching TITAN CUSTOMIX"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
